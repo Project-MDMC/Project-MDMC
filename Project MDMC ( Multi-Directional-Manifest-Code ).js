d501f56b015fec1114e1953afa242a74e931b27b
Project-MDMC <projectmdmc@outlook.com>
2017-05-03 17:30:18 -0400

    Add files via upload

---
+++ Project MDMC ( Multi-Directional-Manifest-Code ).js
@@ -0,0 +1,7 @@
+/*
+* _________________________________________________
+*             ---Project MDMC---
+* Welcome to Project MDMC Main Manifest folder!
+* Remember to comeplete this project soon and fast!
+* _________________________________________________
+*/
